fc9f2ce89da8a7a13d45d5610bea0ff3473a60e7
philippbruhin <info@philippbruhin.ch>
2017-12-29 23:10:13 +0100

    added db context

---
+++ UCmsApi/Data/UCmsApiContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace UCmsApi.Data
+{
+    public class UCmsApiContext : DbContext
+    {
+        public UCmsApiContext(DbContextOptions<UCmsApiContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Page> Pages { get; set; }
+        public DbSet<Sidebar> Sidebar { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
