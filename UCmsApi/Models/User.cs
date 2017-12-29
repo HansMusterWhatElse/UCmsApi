588fe76e4f9d467513e46a848399df69155948c1
philippbruhin <info@philippbruhin.ch>
2017-12-29 23:04:05 +0100

    added models

---
+++ UCmsApi/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+
+namespace UCmsApi.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string IsAdmin { get; set; }
+    }
+}
