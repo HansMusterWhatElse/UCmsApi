5c705ec5f75a2f397e5d80bdb477c16a1418a129
philippbruhin <info@philippbruhin.ch>
2017-12-30 23:35:33 +0100

    added page controller

---
+++ UCmsApi/Controllers/PagesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UCmsApi.Data;
+using UCmsApi.Models;
+
+namespace UCmsApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class PagesController : Controller
+    {
+        private readonly UCmsApiContext _context;
+
+        public PagesController(UCmsApiContext context)
+        {
+            _context = context;
+        }
+
+        // GEt api/pages
+        public IActionResult Get()
+        {
+            List<Page> pages = _context.Pages.ToList();
+            return Json(pages);
+        }
+    }
+}
