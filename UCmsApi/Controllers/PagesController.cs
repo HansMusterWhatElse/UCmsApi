c0aef1f2f0f4c7ea40f90efbf3fe7b951e026f55
philippbruhin <info@philippbruhin.ch>
2018-01-04 23:23:20 +0100

    added add page methods

--- UCmsApi/Controllers/PagesController.cs
+++ UCmsApi/Controllers/PagesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using UCmsApi.Data;
 using UCmsApi.Models;
 
@@ -18,11 +19,44 @@ namespace UCmsApi.Controllers
             _context = context;
         }
 
-        // GEt api/pages
+        // GET api/pages
         public IActionResult Get()
         {
             List<Page> pages = _context.Pages.ToList();
             return Json(pages);
+        }
+
+        // GET api/pages/slug
+        [HttpGet("{slug}")]
+        public IActionResult Get(string slug)
+        {
+            Page page = _context.Pages.SingleOrDefault(x => x.Slug == slug);
+            if (page == null)
+            {
+                return Json("PageNotFound");
+            }
+            return Json(page);
+        }
+
+        // POST api/pages/create
+        [HttpPost("create")]
+        public IActionResult create([FromBody]Page page)
+        {
+            page.Slug = page.Title.Replace(" ", "-").ToLower();
+            page.HasSidebar = page.HasSidebar ?? "no";
+
+            var slug = _context.Pages.FirstOrDefault(x => x.Slug == page.Slug);
+            if (slug != null)
+            {
+                return Json("pageExists");
+            }
+            else
+            {
+                _context.Pages.Add(page);
+                _context.SaveChanges();
+
+                return Json("ok");
+            }
         }
     }
 }
--- UCmsApi/Startup.cs
+++ UCmsApi/Startup.cs
@@ -25,6 +25,12 @@ namespace UCmsApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddCors(options => options.AddPolicy("AllowAll",
+                p => p.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()));
+
             services.AddDbContext<UCmsApiContext>(options => options.UseSqlServer
                 (Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
@@ -33,6 +39,7 @@ namespace UCmsApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors("AllowAll");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
